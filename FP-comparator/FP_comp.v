1168cbcd0aa5f3797ff5305d37f0ecac38f44bc9
George Castillo <castillo@devuan.devuan>
2017-01-23 01:51:10 -0700

    Added a floating point comparator.

--- FP-comparator/FP_comp.v
+++ FP-comparator/FP_comp.v
@@ -2,50 +2,45 @@ module FP_comp
   (
    input wire        sign_1, sign_2,    // 1 indicates a negative number
    input wire [3:0]  exp_1, exp_2,
-   input wire [8:0] frac_1, frac_2,
+   input wire [7:0]  frac_1, frac_2,
    output reg        gt                 // 1 if input_1 is greater than input_2
    );
 
    // interim results
-   reg [11:0]        input_1 = {exp_1, frac_1};
-   reg [11:0]        input_2 = {exp_2, frac_2);
+   wire [11:0]       input_1;
+   wire [11:0]       input_2;
 
-   always @*
-     if (sign_1 > sign_2)
-       // implies input_1 < input_2
-       begin
-          gt = 0;
-       end
-     else if (sign_1 < sign_2)
-       // implies input_1 > input_2
-       begin
-          gt = 1;
-       end
-     else
-       // depends on relative magnitudes of the two inputs
-       if (sign_1 == 1'b0')
-         // dealing with positive floating point values
-         begin
-            if (input_1 > input_2)
-              begin
-                gt = 1;
-              end
-            else
-              begin
-                 gt = 0;
-              end
-         end // if (sign_1 == 1'b0')
-       else
-         // dealing with negative floating point values
-         begin
-            if (input_1 < input_2)
-              begin
-                 gt = 1
-              end
-            else
-              begin
-                 gt = 0;
-              end
-         end // else: !if(sign_1 == 1'b0')
+   assign input_1 = {exp_1, frac_1};
+   assign input_2 = {exp_2, frac_2};
+
+   always @* begin
+      if (sign_1 == sign_2) begin
+         if (sign_1 == 1'b0) begin               // positive inputs
+            if (input_1 > input_2) begin
+               gt = 1'b1;
+            end
+            else begin
+               gt = 1'b0;
+            end
+         end
+         else begin                              // negative inputs
+            if (input_1 < input_2) begin
+               gt = 1'b1;
+            end
+            else begin
+               gt = 1'b0;
+            end
+         end // else: !if(sign_1 == 1'b0)
+      end // if (sign_1 == sign_2)
+      else begin                                 // opposite signed inputs
+        $display("the value of sign_1 is: %b", sign_1);
+         if (sign_1 == 1'b1) begin
+            gt = 1'b0;
+         end
+         else if (sign_2 == 1'b1) begin
+            gt = 1'b1;
+         end
+      end // else: !if(sign_1 == sign_2)
+   end // always @ *
 
 endmodule // FP_comp
--- FP-comparator/FP_comp_tb.v
+++ FP-comparator/FP_comp_tb.v
@@ -5,48 +5,104 @@ module FP_comp_tb ();
    reg          sign_1, sign_2;
    reg [3:0]    exp_1, exp_2;
    reg [7:0]    frac_1, frac_2;
+   wire         gt;
 
    FP_comp code_under_test
      (.sign_1(sign_1), .sign_2(sign_2),
       .exp_1(exp_1), .exp_2(exp_2),
-      .frac_1(frac_1), .frac_2(frac));
+      .frac_1(frac_1), .frac_2(frac_2),
+      .gt(gt));
 
-   always
-     #5 clk = !clk;
+   initial begin
+      // testing positive numbers //
 
-   always
-     // start with some positive numbers
-     begin
-     #5
-       // input_1 > input_2 => output should be 1
-       sign_1 = 0;
-        sign_2 = 0;
-        exp_1 = 1;
-        exp_2 = 0;
-        frac_1 = 0.314;
-        frac_2 = 0.314;
+      // input_1 > input_2 => output should be 1
+      sign_1 = 0;
+      sign_2 = 0;
+      exp_1 = 1;
+      exp_2 = 0;
+      frac_1 = 314;
+      frac_2 = 314;
 
-        // input_2 > input_1 => output should be 0
-       sign_1 = 0;
-        sign_2 = 0;
-        exp_1 = 0;
-        exp_2 = 1;
-        frac_1 = 0.314;
-        frac_2 = 0.314;
+      #5
 
-        // input_2 == input_1 => output should be 0
-       sign_1 = 0;
-        sign_2 = 0;
-        exp_1 = 2;
-        exp_2 = 2;
-        frac_1 = 0.314;
-        frac_2 = 0.314;
+      // input_2 > input_1 => output should be 0
+      sign_1 = 0;
+      sign_2 = 0;
+      exp_1 = 0;
+      exp_2 = 1;
+      frac_1 = 314;
+      frac_2 = 314;
 
-     #5
-       // now for some negative numbers
-       sign_1 = 1;
-        sign_2 = 1;
-        exp_1 = 1;
-        exp_2 = 0;
-        frac_1 = 0.314;
-        frac_2 = 0.314;
+      #5
+
+      // input_2 == input_1 => output should be 0
+      sign_1 = 0;
+      sign_2 = 0;
+      exp_1 = 2;
+      exp_2 = 2;
+      frac_1 = 314;
+      frac_2 = 314;
+
+      // testing negative numbers //
+
+      #5
+      // input_2 > input_1 => output should be 0
+      sign_1 = 1;
+      sign_2 = 1;
+      exp_1 = 1;
+      exp_2 = 0;
+      frac_1 = 314;
+      frac_2 = 314;
+
+      #5
+      // input_1 > input_2 => output should be 1
+      sign_1 = 1;
+      sign_2 = 1;
+      exp_1 = 1;
+      exp_2 = 2;
+      frac_1 = 314;
+      frac_2 = 314;
+
+      #5
+      // input_1 == input_2 => output should be 0
+      sign_1 = 1;
+      sign_2 = 1;
+      exp_1 = 1;
+      exp_2 = 1;
+      frac_1 = 314;
+      frac_2 = 314;
+
+
+      // testing opposite signs //
+
+      #5
+      // input_1 > input_2 => output should be 1
+      sign_1 = 0;
+      sign_2 = 1;
+      exp_1 = 1;
+      exp_2 = 2;
+      frac_1 = 314;
+      frac_2 = 314;
+
+      #5
+      // input_2 > input_1 => output should be 0
+      sign_1 = 1;
+      sign_2 = 0;
+      exp_1 = 1;
+      exp_2 = 2;
+      frac_1 = 314;
+      frac_2 = 314;
+
+      #5
+      // input_1 > input_2 => output should be 1
+      sign_1 = 0;
+      sign_2 = 0;
+      exp_1 = 1;
+      exp_2 = 1;
+      frac_1 = 628;
+      frac_2 = 314;
+
+   end
+
+endmodule
