e10e782b254f1cd29ef9382e150a58b06a121b2b
George Castillo <gmcastil@gmail.com>
2017-03-05 01:59:28 -0700

    More mature addition to this FPGA core.

--- stopwatch/stopwatch.v
+++ stopwatch/stopwatch.v
@@ -1,11 +1,13 @@
 module stopwatch
-  (
-   input wire clk,
-   input wire enable,
-   input wire up,
-   input wire reset,
-   output wire [15:0] digits
-   );
+  #(
+    parameter POWER = 23
+    )(
+      input wire         clk,
+      input wire         enable,
+      input wire         up,
+      input wire         reset,
+      output wire [15:0] digits
+      );
 
    localparam DVSR = 10000000;  // this produces a desired tick every 100ms
 
@@ -35,6 +37,69 @@ module stopwatch
 
    // next state logic
 
+   // work out the 100ms tick that everything else is driven by
+   assign timer_next = (timer_reg == DVSR && enable) ? {power{1'b0}} :
+                       (enable) ? timer_reg + 1'b1 :
+                       timer_reg;
+   assign timer_tick = {timer_reg == DVSR) ? 1'b1 : 1'b0;
+
+   always @(*) begin
+      if (enable && timer_tick) begin
+         if (up) begin  // for counting upwards
+            if (bcd_0_reg == NINE) begin
+               bcd_0_next = ZERO;
+               if (bcd_1_reg == NINE) begin
+                  bcd_1_next = ZERO;
+                  if (bcd_2_reg == NINE) begin
+                     bcd_2_next = ZERO;
+                     if (bcd_3_reg == NINE) begin
+                        bcd_3_next = ZERO;
+                     end else begin
+                        bcd_3_next = bcd_3_reg + 1'b1;
+                     end
+                  end else begin
+                     bcd_2_next = bcd_2_reg + 1'b1;
+                  end
+               end else begin
+                  bcd_1_next = bcd_1_reg + 1'b1;
+               end
+            end else begin
+               bcd_0_next = bcd_0_reg +1'b1;
+            end
+         end else begin // now we're counting downwards
+            if (bcd_0_reg == ZERO) begin
+               bcd_0_next = NINE;
+               if (bcd_1_reg == ZERO) begin
+                  bcd_1_next = NINE;
+                  if (bcd_2_reg == ZERO) begin
+                     bcd_2_next = NINE;
+                     if (bcd_3_reg == ZERO) begin
+                        bcd_3_next = NINE;
+                     end else begin
+                        bcd_3_next = bcd_3_reg - 1'b1;
+                     end
+                  end else begin
+                     bcd_2_next = bcd_2_reg - 1'b1;
+                  end
+               end else begin // if (bcd_1_reg == ZERO)
+                  bcd_1_next = bcd_1_reg - 1'b1;
+               end // else: !if(bcd_1_reg == ZERO)
+            end else begin // if (bcd_0_reg == ZERO)
+               bcd_0_next = bcd_0_reg - 1'b1;
+            end // else: !if(bcd_0_reg == ZERO)
+         end // else: !if(up)
+      end // if (enable && timer_tick)
+   end // always @ (*)
+
+
+
+
+
+
+
+
+
+
    // sequential logic
    always @(posedge clk) begin
       if (reset) begin
@@ -51,6 +116,5 @@ module stopwatch
          timer_reg <= timer_next;
       end // else: !if(reset)
    end // always @ (posedge clk)
-
 
    // output logic
