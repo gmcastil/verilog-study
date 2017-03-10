37756b670f15ae0a2b517eafdde4ead5a28d5da5
George Castillo <gmcastil@gmail.com>
2017-03-09 23:51:00 -0700

    Found a bug at the edges of the stack.  Rewriting.

--- stack/stack.v
+++ stack/stack.v
@@ -11,89 +11,76 @@ module stack
    output wire       error
    );
 
-  // Note that the bottom of the stack is at the largest addres
-  localparam BOTTOM_ADDR  = 4'b1111;
-  localparam TOP_ADDR     = 4'b0000;
+   // Note that the bottom of the stack is at the largest addres
+   localparam BOTTOM_ADDR  = 4'b1111;
 
-  // Signal declarations
-  reg                push_reg;
-  reg                pop_reg;
+   // Signal declarations
+   reg                    push_reg;
+   reg                    pop_reg;
 
-  reg [7:0]          register_file [15:0];
-  // Register incoming data and push / pop signals
-  reg [7:0]          data_reg;
-  reg                push_enable;
-  reg                pop_enable;
+   reg [7:0]              register_file [15:0];
+   reg [7:0]              data_reg;                  // incoming data
+   reg                    error_reg;                 // stack under or overflow
+   reg                    error_next;
+   reg [7:0]              read_data_reg;             // store popped data
 
-  // Create a location to store popped data and indicate an under / overflow
-  // condition
-  reg                error_reg;
-  reg                error_next;
-  reg [7:0]          read_data_reg;
+   reg                    push_enable;
+   reg                    pop_enable;
 
-  // Stack pointer
-  reg [3:0]          stack_ptr_reg;
-  reg [3:0]          stack_ptr_next;
+   reg [3:0]              stack_ptr_reg;
+   reg [3:0]              stack_ptr_next;
 
-  // --- Next State Logic
-  always @(*) begin
-    if (push_reg) begin
-      if (stack_ptr_reg == TOP_ADDR) begin
-        // Check for stack overflow condition
-        error_next <= 1'b1;
-        push_enable <= 1'b0;
-        pop_enable <= 1'b0;
-        stack_ptr_next <= stack_ptr_reg;
+   // --- Next State Logic
+   always @(*) begin
+      if (push_reg) begin
+            error_next <= 1'b0;
+            push_enable <= 1'b1;
+            pop_enable <= 1'b0;
+            stack_ptr_next <= stack_ptr_reg - 4'b0001;  // decreasing addresses
+      end else if (pop_reg) begin
+         if (stack_ptr_reg == BOTTOM_ADDR) begin
+            // Check for stack underflow condition
+            error_next <= 1'b1;
+            push_enable <= 1'b0;
+            pop_enable <= 1'b0;
+            stack_ptr_next <= stack_ptr_reg;
+         end else begin
+            error_next <= 1'b0;
+            push_enable <= 1'b0;
+            pop_enable <= 1'b1;
+            stack_ptr_next <= stack_ptr_reg + 4'b0001;  // again, decreasing addresses
+         end
       end else begin
-        error_next <= 1'b0;
-        push_enable <= 1'b1;
-        pop_enable <= 1'b0;
-        stack_ptr_next <= stack_ptr_reg - 4'b0001;  // decreasing addresses
+         error_next <= error_reg;
+         push_enable <= 1'b0;
+         pop_enable <= 1'b0;
+         stack_ptr_next <= stack_ptr_reg;
       end
-    end else if (pop_reg) begin
-      if (stack_ptr_reg == BOTTOM_ADDR) begin
-        // Check for stack underflow condition
-        error_next <= 1'b1;
-        push_enable <= 1'b0;
-        pop_enable <= 1'b0;
-        stack_ptr_next <= stack_ptr_reg;
-      end else begin
-        error_next <= 1'b0;
-        push_enable <= 1'b0;
-        pop_enable <= 1'b1;
-        stack_ptr_next <= stack_ptr_reg + 4'b0001;  // again, decreasing addresses
+   end
+
+   // --- Output Logic
+   assign error = error_reg;
+   assign data_out = read_data_reg;
+
+   // --- Sequential Logic
+   always @(posedge clk) begin
+      stack_ptr_reg <= stack_ptr_next;
+      if (reset) begin
+         read_data_reg <= 8'b0;
+         error_reg <= 1'b0;
+         stack_ptr_reg <= BOTTOM_ADDR;
+      end else if (push_enable) begin
+         register_file[stack_ptr_reg] <= data_reg;
+      end else if (pop_enable) begin
+         read_data_reg <= register_file[stack_ptr_reg + 1'b1];  // use previous address for pops
       end
-    end else begin
-      error_next <= error_reg;
-      push_enable <= 1'b0;
-      pop_enable <= 1'b0;
-      stack_ptr_next <= stack_ptr_reg;
-    end
-  end
+   end
 
-  // --- Output Logic
-  assign error = error_reg;
-  assign data_out = read_data_reg;
-
-  // --- Sequential Logic
-  always @(posedge clk) begin
-    stack_ptr_reg <= stack_ptr_next;
-    if (reset) begin
-      read_data_reg <= 8'b0;
-      error_reg <= 1'b0;
-      stack_ptr_reg <= BOTTOM_ADDR;
-    end else if (push_enable) begin
-      register_file[stack_ptr_reg] <= data_reg;
-    end else if (pop_enable) begin
-      read_data_reg <= register_file[stack_ptr_reg + 1'b1];  // use previous address for pops
-    end
-  end
-
-  // Register inputs to this module
-  always @(posedge clk) begin
-    push_reg <= push;
-    pop_reg <= pop;
-    data_reg <= data_in;
-  end
+   // Register inputs to this module
+   always @(posedge clk) begin
+      push_reg <= push;
+      pop_reg <= pop;
+      data_reg <= data_in;
+   end
 
 endmodule // stack
--- stack/stack_tb.v
+++ stack/stack_tb.v
@@ -14,11 +14,14 @@ module stack_tb ();
    localparam T = 10;  // 10ns clock period
 
    initial begin
-      clk <= 1'b1;
+      clk = 1'b1;
       forever begin
-         #(T/2) clk = ~clk;
+         #(T/2)
+         clk = ~clk;
       end
    end
+
+   int i;
 
    initial begin
       reset = 1'b0;
@@ -27,20 +30,39 @@ module stack_tb ();
       pop = 1'b0;
       push = 1'b0;
       #(2*T);
-      for (i=15; i<=0; i--) begin
+      // Fill up the stack and make sure to try to generate an overflow and
+      // make sure that it correctly identifies stack overflows.
       reset = 1'b0;
-      data_in = 8'b10101010;
+      $display("Testing push behavior...");
+      for (i=0; i<=8; i++) begin
+         data_in = 8'b10101010;
+         pop = 1'b0;
+         push = 1'b1;
+         #T;
+         data_in = 8'b01010101;
+         pop = 1'b0;
+         push = 1'b1;
+         #(2*T);
+      end
+      $display("Testing stack overflow...");
+      data_in = 8'b11111111;
       pop = 1'b0;
       push = 1'b1;
-      #T;
-      pop = 1'b0;
-      push = 1'b0;
       #(2*T);
+      // Now try to empty the stack and then generate an underflow afterwards
+      $display("Testing pop behavior...");
+      for (i=0; i<=16; i++) begin
+         pop = 1'b1;
+         push = 1'b0;
+         #T;
+         pop = 1'b0;
+         push = 1'b0;
+         #(2*T);
+      end
+      $display("Testing stack underflow...");
       pop = 1'b1;
       push = 1'b0;
-      #T;
-      pop = 1'b0;
-      push = 1'b0;
+      #(2*T);
    end
 
    stack
