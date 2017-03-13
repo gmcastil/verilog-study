c4e34d8b24367f4d8a1acebac0c51a3555c2d759
George Castillo <gmcastil@gmail.com>
2017-03-13 01:29:17 -0600

    Debounce circuit and state machine completed.  Working on testbench next.

--- debounce/debounce.sv
+++ debounce/debounce.sv
@@ -3,7 +3,7 @@ module debounce#(
   (
    input      clk,
    input      reset,
-   input      sw,
+              nnnnnn input sw,
    output reg db
    );
 
@@ -32,81 +32,87 @@ module debounce#(
   localparam WAIT0_3_ID  = 6;
   localparam ONE_ID      = 7;
 
+
+
   // --- Combinatorial State Movements
   always @(*) begin
 
     case (1'b1)
 
       present_state[ZERO_ID]: begin
-         if (sw) begin
-            next_state = WAIT1_1;
-         end
+        if (sw) begin
+          next_state = WAIT1_1;
+        end else begin
+          next_state = ZERO;
+        end
       end
 
       present_state[WAIT1_1_ID]: begin
-         if (sw && !m_tick) begin
-            next_state = WAIT1_1;
-         end else if (sw && m_tick) begin
-            next_state = WAIT1_2;
-         end else begin
-            next_state = ZERO_ID;
-         end
+        if (sw && !m_tick) begin
+          next_state = WAIT1_1;
+        end else if (sw && m_tick) begin
+          next_state = WAIT1_2;
+        end else begin
+          next_state = ZERO;
+        end
       end
 
       present_state[WAIT1_2_ID]: begin
-         if (sw && !m_tick) begin
-            next_state = WAIT1_2;
-         end else if (sw && m_tick) begin
-            next_state = WAIT1_3;
-         end else begin
-            next_state = ZERO_ID;
-         end
+        if (sw && !m_tick) begin
+          next_state = WAIT1_2;
+        end else if (sw && m_tick) begin
+          next_state = WAIT1_3;
+        end else begin
+          next_state = ZERO;
+        end
       end
 
       present_state[WAIT1_3_ID]: begin
-         if (sw && !m_tick) begin
-            next_state = WAIT1_3;
-         end else if (sw && m_tick) begin
-            next_state = ONE;
-         end else begin
-            next_state = ZERO_ID;
-         end
+        if (sw && !m_tick) begin
+          next_state = WAIT1_3;
+        end else if (sw && m_tick) begin
+          next_state = ONE;
+        end else begin
+          next_state = ZERO;
+        end
       end
 
       present_state[ONE_ID]: begin
-         if (sw) begin
-            next_state = ONE;
-         end else begin
-            next_state = WAIT;
-         end
+        if (sw) begin
+          next_state = ONE;
+        end else begin
+          next_state = WAIT0_1;
+        end
       end
 
       present_state[WAIT0_1_ID]: begin
-         if (sw && !m_tick) begin
-            next_state = WAIT0_1;
-         end else if (sw && m_tick) begin
-            next_state = WAIT0_2;
-         end else begin
-            next_state = ONE_ID;
-         end
+        if (!sw && !m_tick) begin
+          next_state = WAIT0_1;
+        end else if (!sw && m_tick) begin
+          next_state = WAIT0_2;
+        end else begin
+          next_state = ONE;
+        end
       end
 
       present_state[WAIT0_2_ID]: begin
-         if (sw && !m_tick) begin
-            next_state = WAIT0_2_ID;
-         end else if (sw && m_tick) begin
-            next_state = WAIT0_3_ID;
-         end else begin
-            next_state = ONE_ID;
-         end
+        if (!sw && !m_tick) begin
+          next_state = WAIT0_2;
+        end else if (!sw && m_tick) begin
+          next_state = WAIT0_3;
+        end else begin
+          next_state = ONE;
+        end
       end
 
       present_state[WAIT0_3_ID]: begin
-         if (sw && !m_tick) begin
-            next_state = WAIT0_3_ID;
-         end else if (sw && !m_tick) begin
-            next_state = ZERO_ID;
-         end
+        if (!sw && !m_tick) begin
+          next_state = WAIT0_3;
+        end else if (!sw && m_tick) begin
+          next_state = ZERO;
+        end else begin
+          next_state = ONE;
+        end
       end
 
       default: begin
@@ -116,12 +122,51 @@ module debounce#(
 
   end //always @(*)
 
-  // --- State Machine Signals
+  // --- Present State Register
   always @(posedge clk) begin
+    if (reset) begin
+      present_state <= ZERO;
+    end else begin
+      present_state <= next_state;
+    end
 
-     case (1'b1)
+    case (1'b1)
 
+      present_state[ZERO_ID]: begin
+        db <= 1'b0;
+      end
 
-  end
+      present_state[WAIT1_1_ID]: begin
+        db <= 1'b0;
+      end
+
+      present_state[WAIT1_2_ID]: begin
+        db <= 1'b0;
+      end
+
+      present_state[WAIT1_3_ID]: begin
+        db <= 1'b0;
+      end
+
+      present_state[ONE_ID]: begin
+        db <= 1'b1;
+      end
+
+      present_state[WAIT0_1_ID]: begin
+        db <= 1'b1;
+      end
+
+      present_state[WAIT0_2_ID]: begin
+        db <= 1'b1;
+      end
+
+      present_state[WAIT0_3_ID]: begin
+        db <= 1'b1;
+      end
+
+      default: begin
+      end
+    endcase // case (1'b1)
+  end // always @ (posedge clk)
 
 endmodule // debounce
