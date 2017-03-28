d2b74b99051845b83954c8f98dc2ca177867aab3
George Castillo <gmcastil@gmail.com>
2017-03-28 03:22:15 -0600

    Added debouncing circuit for the reset switch.

--- stack/stack_top.v
+++ stack/stack_top.v
@@ -1,56 +1,73 @@
-module stack_top (
-   input wire       clk,
-   input wire       reset,
-   input wire       push_sw,
-   input wire       pop_sw,
-   input wire [7:0] data_in,
+module stack_top
+  (
+   input wire        clk,
+   input wire        reset_sw,
+   input wire        push_sw,
+   input wire        pop_sw,
+   input wire [7:0]  data_in,
 
    output wire [7:0] data_out,
    output wire       error_out
    );
 
-   wire [4:0] debug_stack_ptr_reg;
-   wire [4:0] debug_stack_ptr_next;
-   wire       debug_push_reg;
-   wire       debug_pop_reg;
+  wire [4:0]         debug_stack_ptr_reg;
+  wire [4:0]         debug_stack_ptr_next;
+  wire               debug_push_reg;
+  wire               debug_pop_reg;
 
-   stack u_stack(
-                 .clk                  (clk),        /* input  */
-                 .reset                (reset),      /* input  */
-                 .push                 (push_db),    /* input  */
-                 .pop                  (pop_db),     /* input  */
-                 .data_in              (data_in),    /* input  */
-                 .data_out             (data_out),   /* output */
-                 .error                (error_out),  /* output */
+  wire               reset_db;
+  wire               push_db;
+  wire               pop_db;
 
-                 .debug_stack_ptr_reg  (debug_stack_ptr_reg),
-                 .debug_stack_ptr_next (debug_stack_ptr_next),
-                 .debug_push_reg       (debug_push_reg),
-                 .debug_pop_reg        (debug_pop_reg)
-                 );
+  stack u_stack
+    (
+     .clk                  (clk),        /* input  */
+     .reset                (reset_db),   /* input  */
+     .push                 (push_db),    /* input  */
+     .pop                  (pop_db),     /* input  */
+     .data_in              (data_in),    /* input  */
+     .data_out             (data_out),   /* output */
+     .error                (error_out),  /* output */
 
-  db_fsm u_debounced_push(
-                          .clk    (clk),        /* input  */
-                          .reset  (reset),      /* input  */
-                          .sw     (push_sw),    /* input  */
-                          .db     (push_db)     /* output */
-                          );
+     .debug_stack_ptr_reg  (debug_stack_ptr_reg),
+     .debug_stack_ptr_next (debug_stack_ptr_next),
+     .debug_push_reg       (debug_push_reg),
+     .debug_pop_reg        (debug_pop_reg)
+     );
 
-   db_fsm u_debounced_pop(
-                         .clk    (clk),         /* input  */
-                         .reset  (reset),       /* input  */
-                         .sw     (pop_sw),      /* input  */
-                         .db     (pop_db)       /* output */
-                         );
+  db_fsm u_debounced_push
+    (
+     .clk    (clk),        /* input  */
+     .reset  (reset_db),   /* input  */
+     .sw     (push_sw),    /* input  */
+     .db     (push_db)     /* output */
+     );
 
-   ila_0 // #(
-     // )
-     ila_0_uc (
-                  .clk       (clk),
-                  .probe0    (debug_stack_ptr_reg),
-                  .probe1    (debug_stack_ptr_next),
-                  .probe2    (debug_push_reg),
-                  .probe3    (debug_pop_reg)
-                  ) /* synthesis syn_noprune=1 syn_preserve=1 syn_keep=1 */;
+  db_fsm u_debounced_pop
+    (
+     .clk    (clk),         /* input  */
+     .reset  (reset_db),    /* input  */
+     .sw     (pop_sw),      /* input  */
+     .db     (pop_db)       /* output */
+     );
+
+  db_fsm u_debounced_reset
+    (
+     .clk    (clk),         /* input  */
+     .reset  (reset_sw),    /* input  */
+     .sw     (reset_sw),    /* input  */
+     .db     (reset_db)     /* output */
+     );
+
+  ila_0 // #(
+    // )
+    ila_0_uc
+      (
+       .clk       (clk),
+       .probe0    (debug_stack_ptr_reg),
+       .probe1    (debug_stack_ptr_next),
+       .probe2    (debug_push_reg),
+       .probe3    (reset_sw)
+       ) /* synthesis syn_noprune=1 syn_preserve=1 syn_keep=1 */;
 
 endmodule
